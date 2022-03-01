b580ad0c8e275a4ee36fc7716848b81c3f3e06ff
olegg <oleg.god.2013@mail.ru>
2022-03-01 13:49:12 +0300

    4.2. Заголовок в файле java [#159036]

--- src/main/java/ru/job4j/Info.java
+++ src/main/java/ru/job4j/Info.java
@@ -2,6 +2,13 @@ package ru.job4j;
 
 public class Info {
     public static void main(String[] args) {
-        System.out.println("01.03.2022");
+        byte age = 72;
+        char gender = 'М';
+        float height = 1.78F;
+        short countOfBricks = 22000;
+        int countOfBacteries = 10000000;
+        long distanceInSpace = 1500000000;
+        boolean workAccess = true;
+        long fileSize = 4000000000L;
     }
 }
