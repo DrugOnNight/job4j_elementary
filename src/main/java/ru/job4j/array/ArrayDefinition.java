4b0455344c873dd8103f3487a80aea3cf43e135b
olegg <oleg.god.2013@mail.ru>
2022-04-10 06:03:13 +0300

    6.0.2. Заполнение массива. [#226300]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -8,5 +8,14 @@ public class ArrayDefinition {
         System.out.println(ages.length);
         System.out.println(surnames.length);
         System.out.println(prices.length);
+        String[] names = new String[4];
+        names[0] = "Oleg";
+        names[1] = "Petr";
+        names[2] = "Ivan";
+        names[3] = "Fedor";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
